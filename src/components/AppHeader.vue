48a4b08594268f207033ccca1ac52f4438df9769
Mariosam2 <marco.mariosaa@gmail.com>
2022-11-14 14:39:02 +0100

    added header component and style

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+<template>
+    <header id="site_header" class="py-5">
+        <div class="container d-flex align-items-center">
+            <img class="img-fluid logo mx-4" src="../assets/img/breaking.ico" alt="">
+            <h1 class="text-white">Breaking Bad API</h1>
+        </div>
+
+    </header>
+    <!-- /#site_header -->
+</template>
+<style lang="scss" scoped>
+.logo {
+    max-width: 80px;
+
+}
+</style>
